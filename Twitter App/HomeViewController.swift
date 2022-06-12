b79550b6a67523e08c9352d0ecb29cd57d5951b2
yuu123456 <yuukunn3777@gmail.com>
2022-06-12 15:01:51 +0900

    Twitterアプリ制作開始

--- Twitter App/HomeViewController.swift
+++ Twitter App/HomeViewController.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var TweetDisplayArea: UITableView!
+    
+    let tweet = ["tweet1","tweet2","tweet3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TweetDisplayArea.dataSource = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = tweet[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweet.count
+    }
+
+    
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
